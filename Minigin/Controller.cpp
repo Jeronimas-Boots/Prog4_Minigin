515669caf7c0568865703b0f12425c95defe9c22
Jeronimas Boots <161573100+Jeronimas-Boots@users.noreply.github.com>
2026-03-11 16:59:39 +0100

    controller with emscripten
    so Xinput does not work for emscripten so when in browser it will use the SDL Gamepad input

--- Minigin/Controller.cpp
+++ Minigin/Controller.cpp
@@ -1,7 +1,12 @@
 #include "Controller.h"
+
+#ifdef _WIN32
 #include <Windows.h>
 #include <Xinput.h>
 #pragma comment(lib, "xinput.lib")
+#else
+#include <SDL.h>
+#endif
 
 namespace dae
 {
@@ -11,6 +16,20 @@ namespace dae
 		explicit ControllerImpl(unsigned int controllerIndex)
 			: m_ControllerIndex(controllerIndex)
 		{
+#ifndef _WIN32
+			// Open SDL gamepad by index
+			m_Gamepad = SDL_OpenGamepad(SDL_GetGamepads(nullptr)[controllerIndex]);
+#endif
+		}
+
+		~ControllerImpl()
+		{
+#ifndef _WIN32
+			if (m_Gamepad)
+			{
+				SDL_CloseGamepad(m_Gamepad);
+			}
+#endif
 		}
 
 		void Update()
@@ -18,6 +37,8 @@ namespace dae
 			m_ButtonsPressedThisFrame = 0;
 			m_ButtonsReleasedThisFrame = 0;
 
+#ifdef _WIN32
+			// XInput implementation (Windows)
 			XINPUT_STATE currentState{};
 			if (XInputGetState(m_ControllerIndex, &currentState) == ERROR_SUCCESS)
 			{
@@ -27,6 +48,35 @@ namespace dae
 
 				m_PreviousState = currentState;
 			}
+#else
+			// SDL implementation (Emscripten/other platforms) I have enlisted the help from LLM's here for I did not know how to make the emscripten work with gamepad
+			if (m_Gamepad)
+			{
+				unsigned int currentState = 0;
+
+				// Map SDL buttons to our button bits
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_DPAD_UP)) currentState |= static_cast<unsigned int>(ControllerButton::DPadUp);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_DPAD_DOWN)) currentState |= static_cast<unsigned int>(ControllerButton::DPadDown);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_DPAD_LEFT)) currentState |= static_cast<unsigned int>(ControllerButton::DPadLeft);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_DPAD_RIGHT)) currentState |= static_cast<unsigned int>(ControllerButton::DPadRight);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_START)) currentState |= static_cast<unsigned int>(ControllerButton::Start);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_BACK)) currentState |= static_cast<unsigned int>(ControllerButton::Back);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_LEFT_STICK)) currentState |= static_cast<unsigned int>(ControllerButton::LeftThumb);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_RIGHT_STICK)) currentState |= static_cast<unsigned int>(ControllerButton::RightThumb);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_LEFT_SHOULDER)) currentState |= static_cast<unsigned int>(ControllerButton::LeftShoulder);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_RIGHT_SHOULDER)) currentState |= static_cast<unsigned int>(ControllerButton::RightShoulder);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_SOUTH)) currentState |= static_cast<unsigned int>(ControllerButton::ButtonA);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_EAST)) currentState |= static_cast<unsigned int>(ControllerButton::ButtonB);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_WEST)) currentState |= static_cast<unsigned int>(ControllerButton::ButtonX);
+				if (SDL_GetGamepadButton(m_Gamepad, SDL_GAMEPAD_BUTTON_NORTH)) currentState |= static_cast<unsigned int>(ControllerButton::ButtonY);
+
+				const auto buttonChanges = m_PreviousButtonState ^ currentState;
+				m_ButtonsPressedThisFrame = buttonChanges & currentState;
+				m_ButtonsReleasedThisFrame = buttonChanges & (~currentState);
+
+				m_PreviousButtonState = currentState;
+			}
+#endif
 		}
 
 		bool IsDownThisFrame(ControllerButton button) const
@@ -41,11 +91,20 @@ namespace dae
 
 		bool IsPressed(ControllerButton button) const
 		{
+#ifdef _WIN32
 			return (m_PreviousState.Gamepad.wButtons & static_cast<unsigned int>(button)) != 0;
+#else
+			return (m_PreviousButtonState & static_cast<unsigned int>(button)) != 0;
+#endif
 		}
 
 	private:
+#ifdef _WIN32
 		XINPUT_STATE m_PreviousState{};
+#else
+		SDL_Gamepad* m_Gamepad{ nullptr };
+		unsigned int m_PreviousButtonState{ 0 };
+#endif
 		unsigned int m_ButtonsPressedThisFrame{ 0 };
 		unsigned int m_ButtonsReleasedThisFrame{ 0 };
 		unsigned int m_ControllerIndex;
