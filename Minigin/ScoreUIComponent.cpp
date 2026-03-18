7ad467701bce91381a9e5fab8919860b196e5cf0
Jeronimas Boots <161573100+Jeronimas-Boots@users.noreply.github.com>
2026-03-18 16:49:04 +0100

    ScoreUiComponent

---
+++ Minigin/ScoreUIComponent.cpp
@@ -0,0 +1,33 @@
+#include "ScoreUIComponent.h"
+#include "ScoreComponent.h"
+#include "TextComponent.h"
+#include "GameObject.h"
+#include "EventID.h"
+dae::ScoreUIComponent::ScoreUIComponent(GameObject* owner, ScoreComponent* scoreComponent)
+	: Component(owner)
+	, m_ScoreComponent(scoreComponent)
+{
+	if (m_ScoreComponent)
+	{
+		m_ScoreComponent->AddObserver(this);
+		Notify(owner, make_sdbm_hash("ScoreChanged"));
+	}
+}
+
+dae::ScoreUIComponent::~ScoreUIComponent()
+{
+	if (m_ScoreComponent)
+		m_ScoreComponent->RemoveObserver(this);
+}
+
+void dae::ScoreUIComponent::Notify(GameObject* object, unsigned int eventId)
+{
+	if (eventId == make_sdbm_hash("ScoreChanged"))
+	{
+		if (!m_TextComponent)
+			m_TextComponent = object->GetComponent<dae::TextComponent>();
+
+		if (m_TextComponent && m_ScoreComponent)
+			m_TextComponent->SetText("Score: " + std::to_string(m_ScoreComponent->GetScore()));
+	}
+}
---
+++ Minigin/ScoreUIComponent.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Component.h"
+#include "Observer.h"
+
+namespace dae
+{
+	class TextComponent;
+	class ScoreComponent;
+
+	class ScoreUIComponent final : public Component, public Observer
+	{
+	public:
+		ScoreUIComponent(GameObject* owner, ScoreComponent* scoreComponent);
+		~ScoreUIComponent() override;
+
+		void Notify(GameObject* object, unsigned int eventId) override;
+
+	private:
+		TextComponent* m_TextComponent{ nullptr };
+		ScoreComponent* m_ScoreComponent{ nullptr };
+	};
+}
